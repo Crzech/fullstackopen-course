c663c6904b6b336da61de23bfaf000f745da2a80
Christian Pernillo <cpernillo11@gmail.com>
2020-07-30 08:21:52 -0600

    step 2

--- part1/courseinformation/src/index.js
+++ part1/courseinformation/src/index.js
@@ -24,6 +24,29 @@ const Total = (props) => (
   <p>Number of exercises {props.total}</p>
 );
 
+const Part = (props) => {
+  switch(props.type) {
+    case 1:
+      return (<Header course={props.course} />);
+    case 2:
+      return (
+        <Content
+          part1={props.part1} 
+          part2={props.part2} 
+          part3={props.part3}
+          exercises1={props.exercises1}
+          exercises2={props.exercises2}
+          exercises3={props.exercises3} 
+        />
+      );
+    case 3:
+      return (
+        <Total 
+          total={props.total}
+        />
+      );
+  }
+}
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -35,18 +58,22 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course} />
-      <Content 
+      <Part type={1} course={course} />
+      <Part
+        type={2}
         part1={part1} 
         part2={part2} 
         part3={part3}
         exercises1={exercises1}
         exercises2={exercises2}
-        exercises3={exercises3}
+        exercises3={exercises3} 
       />
-     <Total total={exercises1 + exercises2 + exercises3} /> 
+      <Part 
+        type={3}
+        total={exercises1 + exercises2 + exercises3}
+      />
     </div>
-  )
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
